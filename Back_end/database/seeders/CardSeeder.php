bf677bf2fbe616f6a091f6d9ad35f07c756a85f0
YoanMln <yoan.moulin@hotmail.fr>
2026-02-14 13:20:49 +0100

    Feat: Contents for seeders

--- Back_end/database/seeders/CardSeeder.php
+++ Back_end/database/seeders/CardSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CardSeeder extends Seeder
 {
@@ -12,6 +13,40 @@ class CardSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('cards')->insert([
+            [
+                'name' => 'Dragon Blanc Aux Yeux Bleus',
+                'image' => 'blue_eyes.jpg',
+                'description' => 'Ce dragon légendaire est un puissant moteur de destruction. Rares sont ceux qui ont survécu à cette terrifiante créature quasiment invincible pour en parler.',
+                'level' => 8,
+                'attack' => 3000,
+                'defense' => 2500,
+                'type_id' => 1, // Monster
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Trou Noir',
+                'image' => 'dark_hole.jpg',
+                'description' => 'Détruisez tous les monstres sur le Terrain',
+                'level' => null,
+                'attack' => null,
+                'defense' => null,
+                'type_id' => 2, // Spell
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Force de Miroir',
+                'image' => 'mirror_force.jpg',
+                'description' => 'Lorsqu un monstre de l adversaire déclare une attaque : détruisez tous les monstres en Position d Attaque de votre adversaire.',
+                'level' => null,
+                'attack' => null,
+                'defense' => null,
+                'type_id' => 3, // Trap
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
     }
 }
--- Back_end/database/seeders/TypeSeeder.php
+++ Back_end/database/seeders/TypeSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
+use function Symfony\Component\Clock\now;
 
 class TypeSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('types')->insert([
+            ['name' => 'Monster', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Spell', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Trap', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 }
