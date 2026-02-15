0895a107e8d177ef6d1faa57ce149f9fe471d2b2
YoanMln <yoan.moulin@hotmail.fr>
2026-02-15 14:08:23 +0100

    Refactor : Rename file

--- Front_end/src/services/fetchAPI.js
+++
@@ -1,9 +0,0 @@
-const API_URL = "http://127.0.0.1:8000/api";
-
-export const fetchCards = async () => {
-  const response = await fetch(`${API_URL}/cards`);
-  if (!response.ok) {
-    throw new Error("Erreur API");
-  }
-  return await response.json();
-};
