8f8fbe1a89ac163af0a3c405c1cac349588911c5
YoanMln <yoan.moulin@hotmail.fr>
2026-02-28 18:07:27 +0100

    Feat : Add computed to clean card types

--- Front_end/src/components/CardInfos.vue
+++ Front_end/src/components/CardInfos.vue
@@ -1,19 +1,25 @@
 <script setup>
+import { computed } from "vue";
 const props = defineProps({
   card: {
     type: Object,
     required: true,
   },
 });
+const displayTypes = computed(() => {
+  return [
+    props.card.type?.name,
+    props.card.monster_type?.name,
+    props.card.monster_primary_type?.name,
+    props.card_monster_secondary_type?.name,
+  ]
+    .filter(Boolean)
+    .join(" / ");
+});
 </script>
 
 <template>
-
   <h3 class="card-name">{{ card.name }}</h3>
 
-  <p class="card-type">
-    Type : {{ card.type?.name }} / {{ card.monster_type?.name }} /
-    {{ card.monster_primary_type?.name }} /
-    {{ card.monster_secondary_type?.name }}
-  </p>
+  <p class="card-type">Type : {{ displayTypes }}</p>
 </template>
