22dc213e1277e901ed4fdafb7f13cabc843ae050
YoanMln <yoan.moulin@hotmail.fr>
2026-03-09 21:12:10 +0100

    Feat : Install and setup Pinia

--- Front_end/src/main.js
+++ Front_end/src/main.js
@@ -2,5 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./Router/router";
 import "@/assets/scss/main.scss";
+import {createPinia} from "pinia";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(router);
+app.use(pinia);
+
+
+app.mount("#app");
