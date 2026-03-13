f9c0852d1556dec4649f80ecff5b30916afd60ac
YoanMln <yoan.moulin@hotmail.fr>
2026-03-13 18:03:45 +0100

    Refactor : Update cardStore with all filter card type

--- Front_end/src/stores/cardStore.js
+++ Front_end/src/stores/cardStore.js
@@ -12,16 +12,35 @@ export const useCardStore = defineStore("cardStore", () => {
         .toLowerCase()
         .includes(search.value.toLowerCase());
 
-      const matchType = selectedType.value
-        ? [
-            card.monster_primary_type?.name,
-            card.monster_secondary_type?.name,
-            card.monster_tertiary_type?.name,
-            card.type.name,
-          ].includes(selectedType.value)
-        : true;
+      let matchType = true;
+
+      if (selectedType.value === "spell_continue") {
+        matchType =
+          card.type?.name === "Magie" && card.spell_type?.name === "Continue";
+      } else if (selectedType.value === "spell_speed") {
+        matchType =
+          card.type?.name === "Magie" && card.spell_type?.name === "Rapide";
+      } else if (selectedType.value === "field_spell") {
+        matchType =
+          card.type?.name === "Magie" && card.spell_type?.name === "Terrain";
+      } else if (selectedType.value === "ritual_spell") {
+        matchType =
+          card.type?.name === "Magie" && card.spell_type?.name === "Rituelle";
+      } else if (selectedType.value === "trap_continue") {
+        matchType =
+          card.type?.name === "Piège" && card.trap_type?.name === "Continue";
+      } else if (selectedType.value) {
+        matchType = [
+          card.type?.name,
+          card.monster_primary_type?.name,
+          card.monster_secondary_type?.name,
+          card.monster_tertiary_type?.name,
+        ].includes(selectedType.value);
+      }
+
       return matchSearch && matchType;
     });
   });
+
   return { cards, search, selectedType, filteredCards };
 });
