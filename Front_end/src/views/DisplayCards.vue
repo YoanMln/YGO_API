4ab3db1c64fca759b423f0c5f137995c1946d005
YoanMln <yoan.moulin@hotmail.fr>
2026-03-16 16:50:43 +0100

    Feat : Add input selectedType

--- Front_end/src/views/DisplayCards.vue
+++ Front_end/src/views/DisplayCards.vue
@@ -33,6 +33,9 @@ const cardStore = useCardStore();
     v-model="cardStore.search"
     placeholder="Rechercher une carte..."
   />
+  <button class="reset-button" @click="cardStore.resetFilters()">
+    Réinitialiser les filtres
+  </button>
   <div class="container-search-type">
     <div class="container-input">
       <label class="radio">
@@ -108,11 +111,113 @@ const cardStore = useCardStore();
         <input
           class="input-type"
           type="radio"
+          value="spell_continue"
+          v-model="cardStore.selectedType"
+        />
+        Magie Continue
+      </label>
+
+      <label class="radio">
+        <input
+          class="input-type"
+          type="radio"
+          value="spell_speed"
+          v-model="cardStore.selectedType"
+        />
+        Magie Rapide
+      </label>
+
+      <label class="radio">
+        <input
+          class="input-type"
+          type="radio"
+          value="field_spell"
+          v-model="cardStore.selectedType"
+        />
+        Magie Terrain
+      </label>
+
+      <label class="radio">
+        <input
+          class="input-type"
+          type="radio"
+          value="ritual_spell"
+          v-model="cardStore.selectedType"
+        />
+        Magie Rituelle
+      </label>
+
+      <label class="radio">
+        <input
+          class="input-type"
+          type="radio"
+          value="trap_continue"
+          v-model="cardStore.selectedType"
+        />
+        Piège Continue
+      </label>
+      <label class="radio">
+        <input
+          class="input-type"
+          type="radio"
+          value="counter_trap"
+          v-model="cardStore.selectedType"
+        />
+        Piège Contre
+      </label>
+      <label class="radio">
+        <input
+          class="input-type"
+          type="radio"
           value=""
           v-model="cardStore.selectedType"
         />
         Toutes les cartes
       </label>
+    </div>
+    <div class="container-filter-attributes">
+      <img
+        class="icon-attribute-filter"
+        src="/src/assets/icons/attributes-icons/divin.webp"
+        @click="cardStore.selectedAttribute = 'Divin'"
+        alt="Filtre attribut Divin"
+      />
+      <img
+        class="icon-attribute-filter"
+        src="/src/assets/icons/attributes-icons/earth.webp"
+        @click="cardStore.selectedAttribute = 'Terre'"
+        alt="Filtre attribut Terre"
+      />
+      <img
+        class="icon-attribute-filter"
+        src="/src/assets/icons/attributes-icons/fire.webp"
+        @click="cardStore.selectedAttribute = 'Feu'"
+        alt="Filtre attribut Feu"
+      />
+      <img
+        class="icon-attribute-filter"
+        src="/src/assets/icons/attributes-icons/light.webp"
+        @click="cardStore.selectedAttribute = 'Lumière'"
+        alt="Filtre attribut Lumière"
+      />
+      <img
+        class="icon-attribute-filter"
+        src="/src/assets/icons/attributes-icons/shadow.webp"
+        @click="cardStore.selectedAttribute = 'Ténèbres'"
+        alt="Filtre attribut Ténèbres"
+      />
+      <img
+        class="icon-attribute-filter"
+        src="/src/assets/icons/attributes-icons/water.webp"
+        @click="cardStore.selectedAttribute = 'Eau'"
+        alt="Filtre attribut Eau"
+      />
+      <img
+        class="icon-attribute-filter"
+        src="/src/assets/icons/attributes-icons/wind.webp"
+        @click="cardStore.selectedAttribute = 'Vent'"
+        alt="Filtre attribut Vent"
+      />
     </div>
   </div>
   <div v-if="isLoading">Chargement...</div>
