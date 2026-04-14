9a90cad5f412c79c01b694807003366bbe5e04e8
YoanMln <yoan.moulin@hotmail.fr>
2026-04-14 14:29:07 +0200

    Feat : Add dynamic type display for cards

--- Front_end/src/components/CardInfos.vue
+++ Front_end/src/components/CardInfos.vue
@@ -7,19 +7,33 @@ const props = defineProps({
   },
 });
 const displayTypes = computed(() => {
-  return [
-    props.card.type?.name,
-    props.card.monster_type?.name,
-    props.card.monster_primary_type?.name,
-    props.card_monster_secondary_type?.name,
-  ]
-    .filter(Boolean)
-    .join(" / ");
+  const typeName = props.card.type?.name;
+
+  if (typeName === "Monstre") {
+    return [
+      typeName,
+      props.card.monster_type?.name,
+      props.card.monster_primary_type?.name,
+      props.card.monster_secondary_type?.name,
+    ]
+      .filter(Boolean)
+      .join(" / ");
+  }
+
+  if (typeName === "Magie") {
+    return [typeName, props.card.spellType].filter(Boolean).join(" ");
+  }
+
+  if (typeName === "Piège") {
+    return [typeName, props.card.trapType].filter(Boolean).join(" ");
+  }
+
+  return typeName || "";
 });
 </script>
 
 <template>
   <h3 class="card-name">{{ card.name }}</h3>
-
+  <p class="card-attribute">{{ card.attribute?.name }}</p>
   <p class="card-type">Type : {{ displayTypes }}</p>
 </template>
